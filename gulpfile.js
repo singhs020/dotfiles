16da3fcf2e1d92a74c0854e1b3d7976e4faebf27
Simranjeet Singh <12469462+singhs020@users.noreply.github.com>
2020-02-25 09:52:36 +0530

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,15 @@
+const {src, series, task} = require("gulp");
+const eslint = require("gulp-eslint");
+const shell = require("gulp-shell");
 
+function lint() {
+  return src(["src/**/*.js", "test/**/*.spec.js"])
+  .pipe(eslint())
+  .pipe(eslint.format())
+  .pipe(eslint.failAfterError());
+}
+
+task("test", shell.task("nyc mocha 'test/**/*.spec.js'"));
+
+exports.default = series([lint, "test"]);
+exports.lint = lint;
